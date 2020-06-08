6b84a3d70e393d5ccde344a391c1a7c5c8bfbaf4
eremin1300 <eremin1300@gmail.com>
2020-06-08 15:14:24 +0700

    0.03

--- src/main/java/training/demo/controllers/MainWebController.java
+++ src/main/java/training/demo/controllers/MainWebController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainWebController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("title", "Главная страница");
         return "home";
     }
+    @GetMapping("/cabinet")
+    public String cabinet(Model model) {
+        model.addAttribute("title", "Личный кабинет");
+        return "cabinet";
+        }
 
 }
