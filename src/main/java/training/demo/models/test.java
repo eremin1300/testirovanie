bb76dadc722070ebf8270d9c01550b33b04cd6ba
eremin1300 <eremin1300@gmail.com>
2020-06-15 11:15:28 +0700

    v0.13

---
+++ src/main/java/training/demo/models/test.java
@@ -0,0 +1,51 @@
+package training.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class test {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+    private String fullText;
+}
--- src/main/java/training/demo/repo/postRepository.java
+++ src/main/java/training/demo/repo/postRepository.java
@@ -5,3 +5,7 @@ import training.demo.models.posts;
 
 public interface postRepository extends CrudRepository <posts, Long> {
 }
+public interface videoRepository extends CrudRepository <video, Long> {
+}
+public interface testRepository extends CrudRepository <test, Long> {
+}
