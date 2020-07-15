df49ae0cc5c516adecfa9278680c400a6a3a48bd
eremin1300 <eremin1300@gmail.com>
2020-07-15 23:04:05 +0700

    v 0.47.4

--- src/main/java/training/demo/models/quests.java
+++ src/main/java/training/demo/models/quests.java
@@ -8,9 +8,53 @@ import javax.persistence.Id;
 @Entity
 public class quests {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String quest;
+    private String ans;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuest() {
+        return quest;
+    }
+
+    public void setQuest(String quest) {
+        this.quest = quest;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+
+    public quests(String name, String quest, String ans) {
+        this.name = name;
+        this.quest = quest;
+        this.ans = ans;
+    }
+
+    public quests() {
+    }
 }
 
 
